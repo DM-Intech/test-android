73e9c0c639df107d36de8d8d725a63ad1237e36c
Ehanno <ehanno@intechinfo.fr>
2018-09-24 15:33:04 +0200

    cleaning the code & adding the true false response

--- app/src/main/java/cours_android/intech/cat_quizz/MainActivity.java
+++ app/src/main/java/cours_android/intech/cat_quizz/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,29 +28,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         InputStream is = getResources().openRawResource(R.raw.quiz);
-        TextView text = findViewById(R.id.question);
-        Button bt1 = findViewById(R.id.resp1);
-        Button bt2 = findViewById(R.id.resp2);
-        Button bt3 = findViewById(R.id.resp3);
-        Button bt4 = findViewById(R.id.resp4);
+        final TextView text = findViewById(R.id.question);
+        int[]  ids = new int[]{R.id.resp1, R.id.resp2, R.id.resp3, R.id.resp4};
 
         try {
             ReadJson json = new ObjectMapper().readValue(is,ReadJson.class);
-            int i = 0;
-            for (Question a : json.getQuestions()) {
-                i++;
-                Log.i("question "+ i, a.getQuestion());
-            };
+
             mylist = json.getQuestions();
-            answerList = mylist.get(0).getAnswers();
-            text.setText(mylist.get(0).getGoodAnswer());
-            bt1.setText(answerList[0]);
-            bt2.setText(answerList[1]);
-            bt3.setText(answerList[2]);
-            bt4.setText(answerList[3]);
+            answerList = mylist.get(1).getAnswers();
+            text.setText(mylist.get(1).getGoodAnswer());
+            for(int i = 0; i < ids.length; i++) {
+
+                final Button temp = findViewById(ids[i]);
+                temp.setText(answerList[i]);
+                temp.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        verifyAnswer("" + temp.getText(), mylist.get(1).getGoodAnswer());
+                    }
+                });
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+    }
+    private Boolean verifyAnswer(String answer, String goodAnswer){
+        if(answer == goodAnswer) {
+            // good answer action
+            return true;
+        }
+        else {
+            // bad answer action
+            return false;
+        }
     }
 }
