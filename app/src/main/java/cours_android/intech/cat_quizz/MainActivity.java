ef6732a838f81e0499475758685566300da8f299
Ehanno <ehanno@intechinfo.fr>
2018-09-24 14:42:06 +0200

    adding the text changing for questions

--- app/src/main/java/cours_android/intech/cat_quizz/MainActivity.java
+++ app/src/main/java/cours_android/intech/cat_quizz/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -15,12 +18,20 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    List<Question> mylist = new ArrayList<Question>();
+    String[] answerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quiz);
 
         InputStream is = getResources().openRawResource(R.raw.quiz);
+        TextView text = findViewById(R.id.question);
+        Button bt1 = findViewById(R.id.resp1);
+        Button bt2 = findViewById(R.id.resp2);
+        Button bt3 = findViewById(R.id.resp3);
+        Button bt4 = findViewById(R.id.resp4);
 
         try {
             ReadJson json = new ObjectMapper().readValue(is,ReadJson.class);
@@ -29,11 +40,16 @@ public class MainActivity extends AppCompatActivity {
                 i++;
                 Log.i("question "+ i, a.getQuestion());
             };
-
+            mylist = json.getQuestions();
+            answerList = mylist.get(0).getAnswers();
+            text.setText(mylist.get(0).getGoodAnswer());
+            bt1.setText(answerList[0]);
+            bt2.setText(answerList[1]);
+            bt3.setText(answerList[2]);
+            bt4.setText(answerList[3]);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        List<Question> QList = new ArrayList<Question>();
 
     }
 }
