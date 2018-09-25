782cf8a483bb6559a16ecf04f8d96d6413d93cd3
Ehanno <ehanno@intechinfo.fr>
2018-09-25 10:39:31 +0200

    adding the Button id's system

--- app/src/main/java/cours_android/intech/cat_quizz/MainActivity.java
+++ app/src/main/java/cours_android/intech/cat_quizz/MainActivity.java
@@ -1,5 +1,9 @@
 package cours_android.intech.cat_quizz;
 
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
@@ -8,7 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.os.Vibrator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -30,13 +34,15 @@ public class MainActivity extends AppCompatActivity {
         InputStream is = getResources().openRawResource(R.raw.quiz);
         final TextView text = findViewById(R.id.question);
         int[]  ids = new int[]{R.id.resp1, R.id.resp2, R.id.resp3, R.id.resp4};
+        int random = 2;
+        final int answerBTid = ids[random];
 
         try {
             ReadJson json = new ObjectMapper().readValue(is,ReadJson.class);
 
             mylist = json.getQuestions();
             answerList = mylist.get(1).getAnswers();
-            text.setText(mylist.get(1).getGoodAnswer());
+            text.setText(mylist.get(1).getQuestion());
             for(int i = 0; i < ids.length; i++) {
 
                 final Button temp = findViewById(ids[i]);
@@ -44,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
                 temp.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        verifyAnswer("" + temp.getText(), mylist.get(1).getGoodAnswer());
+                        verifyAnswer(answerBTid, v.getId());
                     }
                 });
             }
@@ -52,14 +58,23 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    private Boolean verifyAnswer(String answer, String goodAnswer){
-        if(answer == goodAnswer) {
+    private void verifyAnswer(int answerBTid, int curentBTid){
+        if(answerBTid == curentBTid) {
             // good answer action
-            return true;
         }
         else {
-            // bad answer action
-            return false;
+            //sond plays + cat image change
+            MakeVibrate(500);
+        }
+    }
+    public void MakeVibrate(int mllisecond) {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(mllisecond);
         }
     }
 }
